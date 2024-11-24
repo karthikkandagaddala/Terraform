09e6643866ce71258c021443fa72d4329cc8de37
karthikkandagaddala <kandagaddala.karthik888@gmail.com>
2024-11-24 20:00:42 +0530

    practice

--- ec2/00-practice.tf
+++ ec2/00-practice.tf
@@ -3,26 +3,26 @@ resource "aws_instance" "web" {
     vpc_security_group_ids = [aws_security_group.allow_ssh1.id]
     instance_type = "t3.micro"
     tags = {
-        name = "db"
+        name = "web"
     }
-    resource "aws_security_group" "allow_ssh1" {
+}
+resource "aws_security_group" "allow_ssh1" {
+    name = "allow_ssh1"
+    description = "Allow inbound SSH traffic"
+    ingress {
+        from_port    = 22
+        to_port      = 22
+        protocol     = "tcp"
+        cidr_blocks  = ["0.0.0.0/0"]
+    }
+    egress {
+       from_port    = 0
+        to_port      = 0
+        protocol     = "-1"
+        cidr_blocks  = ["0.0.0.0/0"]
+    }
+    tags {
         name = "allow_ssh1"
-        description = "Allow inbound SSH traffic"
-        ingress {
-            from_port = 22
-            to_port = 22
-            protocol = "tcp"
-            cidr_blocks = ["0.0.0.0/0"]
-        }
-        egress {
-            from_port = 0
-            to_port = 0
-            protocol = "-1"
-            cidr_blocks = ["0.0.0.0/0"]
-        }
-        tags {
-            name = "allow_ssh1"
-            createdBy = "karthik"
-        }
+        createdBy = "karthik"
     }
 }
