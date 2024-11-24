98abb45b633f96076f0a44cceb8a4a9fa43917bc
karthikkandagaddala <kandagaddala.karthik888@gmail.com>
2024-11-24 23:02:45 +0530

    practice

--- ec2/00-practice.tf
+++ ec2/00-practice.tf
@@ -1,28 +1,28 @@
 resource "aws_instance" "web" {
     ami = "ami-09c813fb71547fc4f"
-    vpc_security_group_ids = [aws_security_group.allow_ssh1.id]
+    vpc_security_group_ids = [aws_security_group.terra.id]
     instance_type = "t3.micro"
     tags = {
-        Name = "web"
+        Name = "web" 
     }
 }
-resource "aws_security_group" "allow_ssh1" {
-    name = "allow_ssh1"
-    description = "Allow inbound SSH traffic"
+resource "aws_security_group" "terra" {
+    name = "terra"
+    description = To access instance_type
     ingress {
-        from_port    = 22
-        to_port      = 22
-        protocol     = "tcp"
-        cidr_blocks  = ["0.0.0.0/0"]
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = [0.0.0.0/0]
     }
     egress {
-       from_port    = 0
-        to_port      = 0
-        protocol     = "-1"
-        cidr_blocks  = ["0.0.0.0/0"]
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = [0.0.0.0/0]
     }
     tags = {
-        Name = "allow_ssh1"
-        CreatedBy = "karthik"
+        Name = "terra"
+        createdBy = "karthik"
     }
 }
