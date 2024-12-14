287e2ab53360218a815ef4805e83c508ee0820c6
karthikkandagaddala <kandagaddala.karthik888@gmail.com>
2024-12-14 23:38:46 +0530

    variables examples

--- ec2/01-variables.tf/variables-code.tf
+++ ec2/01-variables.tf/variables-code.tf
@@ -1,28 +1,28 @@
-resource "aws_instance" "jump-host" {
-    ami = "ami-09c813fb71547fc4f"
-    vpc_security_group_ids = [aws_security_group.dec14.id]
+resource "aws_instance" "Frontend" {
+    vpc_security_group_ids = [aws_security_group.ids.temp]
     instance_type = "t3.micro"
+    ami = "ami-09c813fb71547fc4f"
     tags = {
-        Name = "JUMP-HOST"
+        Name = "Frontend"
     }
 }
-resource "aws_security_group" "dec14" {
-    name = "dec14"
-    description = "allow dec14"
+resource "aws_security_group" "temp" {
+    name = "temp"
+    description = "allow temp"
     ingress {
         from_port = 22
-        to_port =22
+        to_port = 22
         protocol = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
     }
     egress {
         from_port = 0
         to_port = 0
-        protocol = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
+        protocol = -1
+        cidr_blocks = [0.0.0.0/0]
     }
-    tags = {
-        Name = "Jump-Host"
-        createdBy = "kandagaddala.karthik"
+    tags {
+        Name = "Temp"
+        createdBY = "karthik"
     }
 }
