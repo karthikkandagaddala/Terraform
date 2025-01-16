34626711caaba5a00fe2a5d384e65b0e755f2b71
karthikkandagaddala <kandagaddala.karthik888@gmail.com>
2025-01-16 19:36:12 +0530

    variables examples

---
+++ ec2/05-Data-source.tf/ec2.tf
@@ -0,0 +1,28 @@
+resource "aws_instance" "source" {
+    ami = "ami-09c813fb71547fc4f"
+    vpc_security_group_id = [aws_security_group.data]
+    instance_type = "t3.micro"
+    tags = {
+        Name = "source"
+    }
+}
+resource "aws_security_group" "data" {
+    name = "data"
+    decription = "access for ssh connection"
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    tags = {
+        Name = "source"
+        createdBY = "karthik"
+    }
+}
